160a4757cd424989774d0a72944652317253ce81
ESAC Science Data Centre <archives.esac.esa.int@gmail.com>
2019-05-08 17:03:17 +0200

    Delete fastInstall.sh

--- fastInstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "installing"
-#npm install
-#npm i @jupyterlab/coreutils
-python setup.py install
-pip install .
-jupyter nbextension install --py --sys-prefix pyesasky
-jupyter nbextension enable --py --sys-prefix pyesasky
