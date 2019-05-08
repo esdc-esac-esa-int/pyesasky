d12607046741b888397fa07b295f478528b92f4d
ESAC Science Data Centre <archives.esac.esa.int@gmail.com>
2019-05-08 17:07:20 +0200

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -6,18 +6,12 @@
 # uninstall
 echo "uninstalling current version"
 pip uninstall pyesasky
-python setup.py clean
-npm run clean
 jupyter nbextension uninstall pyesasky
 
 sleep 2
 
 # install
-
-echo "installing"
-npm install
-npm i @jupyterlab/coreutils
-python setup.py install
+echo "installing pyesaky"
 pip install .
 jupyter nbextension install --py --sys-prefix pyesasky
 jupyter nbextension enable --py --sys-prefix pyesasky
