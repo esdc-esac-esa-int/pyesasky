5f40be5b1b535cad233ca3c9ec47fc2457aec811
Mattias Wångblad <mattias@winterway.eu>
2019-09-11 15:38:36 +0200

    Updated to version 1.1.0

--- pyesasky/pyesasky.py
+++ pyesasky/pyesasky.py
@@ -27,8 +27,8 @@ class ESASkyWidget(widgets.DOMWidget):
     _model_name = Unicode('ESASkyJSModel').tag(sync=True)
     _view_module = Unicode('pyesasky').tag(sync=True)
     _model_module = Unicode('pyesasky').tag(sync=True)
-    _view_module_version = Unicode('1.0.3').tag(sync=True)
-    _model_module_version = Unicode('1.0.3').tag(sync=True)
+    _view_module_version = Unicode('1.1.0').tag(sync=True)
+    _model_module_version = Unicode('1.1.0').tag(sync=True)
     _view_module_ids = List().tag(sync=True)
     
     def __init__(self):
