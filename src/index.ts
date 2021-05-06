fb1f33b821ceab5de848ed975f2b977aa0eab3d4
Henrik Norman <honke.norman@gmail.com>
2021-05-06 09:27:01 +0200

    converted to typeScript

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+// import {
+//   JupyterFrontEnd,
+//   JupyterFrontEndPlugin
+// } from '@jupyterlab/application';
 // import * as base from "@jupyter-widgets/base";
 /**
  * Initialization data for the pyesasky extension.
  */
- export * from './pyesasky';
+export * from './pyesasky';
 
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'pyesasky',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension pyesasky is activated!');
-    app.activatePlugin('pyesasky');
-    // widgets.registerWidget({
-    //   name: "pyesasky",
-    //   version: '1.8.1',
-    //   exports: null
-    //  });
-  }
-};
+// const extension: JupyterFrontEndPlugin<void> = {
+//   id: 'pyesasky',
+//   autoStart: true,
+//   activate: (app: JupyterFrontEnd) => {
+//     console.log('JupyterLab extension pyesasky is activated!');
+//     app.activatePlugin('pyesasky');
+//     // widgets.registerWidget({
+//     //   name: "pyesasky",
+//     //   version: '1.8.1',
+//     //   exports: null
+//     //  });
+//   }
+// };
 
-export default extension;
+// export default extension;
